b4b06fdff2756e91a5c97a28dd4658e397aaa0f5
Leomaris Reyes <reyes.leomaris@gmail.com>
2022-01-11 18:42:40 -0400

    Replicating App Login UI in Xamarin Forms

---
+++ AppLoginScreen/LogInPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AppLoginScreen
+{
+    public partial class LogInPage : ContentPage
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
